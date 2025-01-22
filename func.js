9a445c9040a4a6c63c947109398a5c6b8510685b
isaccog <isaccogavazzi@me.com>
2025-01-22 14:40:44 +0100

    Update func.js

--- func.js
+++ func.js
@@ -1,1 +1,13 @@
+   <script>
+  fetch('header.html')
+    .then(resp => resp.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    });
 
+  fetch('footer.html')
+    .then(resp => resp.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
+    });
+</script>
