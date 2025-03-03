46127f8ea10ad77adaa535c482dbdef0fa282b66
isaccog <isaccogavazzi@me.com>
2025-03-03 13:14:50 +0100

    Aggiunta articoli via appwrite - prova su blog_new.html
    Aggiunta articoli via appwrite

--- func.js
+++ func.js
@@ -1,29 +1,8 @@
-// Importa i moduli necessari (assicurati di averli installati o usa lo script CDN)
+// Definizione della funzione getRecordsAppWrite
 
-import { Client, Databases, Query } from 'appwrite';
 
-async function getRecordsAppWrite(Id=Null,project='6790f26d002cb405c0af', db='67b9b2060006cbdb292d', collection='67b9b23a00059abb3c7d')
-  {
-  // Inizializza il client Appwrite
-  const client = new Client();
-  client
-    .setEndpoint('https://cloud.appwrite.io/v1')  // Sostituisci con il tuo endpoint, se diverso
-    .setProject(project);              // Sostituisci con il tuo Project ID
-
-  // Inizializza il servizio Database
-  const databases = new Databases(client);
-  let query=[]
-  if Id
-    {
-      query.push(Query.equal('$Id',Id))
-    }
-
-    return await databases.listDocuments(db, collection, query)  
-      
-    
-  }
-
-  await getDocument()  fetch('/header.html')
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('/header.html')
     .then(resp => resp.text())
     .then(data => {
       document.getElementById('header').innerHTML = data;
@@ -34,3 +13,21 @@ async function getRecordsAppWrite(Id=Null,project='6790f26d002cb405c0af', db='67
     .then(data => {
       document.getElementById('footer').innerHTML = data;
     });
+});
+
+async function getRecordsAppWrite(id = null, project = '6790f26d002cb405c0af', db = '67b9b2060006cbdb292d', collection = '67b9b23a00059abb3c7d') {
+  const client = new Appwrite.Client();
+  client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(project);
+
+  const databases = new Appwrite.Databases(client);
+  let query = [];
+  if (id) {
+    query.push(Appwrite.Query.equal('$id', id));
+  }
+  return await databases.listDocuments(db, collection, query);
+}
+
+// Rendi la funzione disponibile globalmente
+window.getRecordsAppWrite = getRecordsAppWrite;
